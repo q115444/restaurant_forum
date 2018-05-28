dcae8fe5fd19be43ca768b98309df1e95152e2b8
a115444 <q115444@gmail.com>
2018-05-29 01:03:30 +0800

    add table style for admin restaurants/index

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -53,15 +53,17 @@ class Admin::RestaurantsController < ApplicationController
   end
 
   private
- 
 
+  def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+  end
+  
  def restaurant_params
     params.require(:restaurant).permit(:name, :opening_hours, :tel, :address, :description, :image)
   end
 
-  def set_restaurant
-   @restaurant= Restaurant.find(params[:id])
-  end
+  
+
 
 end
 
