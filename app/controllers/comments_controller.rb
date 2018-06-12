330c80025ec0f104b712758b47739e587950c598
a115444 <q115444@gmail.com>
2018-06-12 20:00:20 +0800

    admin can delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,17 @@ def create
     redirect_to restaurant_path(@restaurant)
   end
 
+def destroy
+   @restaurant = Restaurant.find(params[:restaurant_id])
+   @comment = Comment.find(params[:id])
+
+    if current_user.admin?
+      @comment.destroy
+      flash[:alert] = "comment was successfully deleted "
+      redirect_to restaurant_path(@restaurant)
+    end
+ end  
+
   private
 
   def comment_params
