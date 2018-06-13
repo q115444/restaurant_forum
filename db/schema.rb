1b3c42a0c120bbde2032577ab078f1c8080e89b7
a115444 <q115444@gmail.com>
2018-06-13 10:23:16 +0800

    add attributes to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180610145737) do
+ActiveRecord::Schema.define(version: 20180613021930) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -60,6 +60,9 @@ ActiveRecord::Schema.define(version: 20180610145737) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "intro"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
