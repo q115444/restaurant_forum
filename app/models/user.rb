99b0927b250a566cb9d2fb35c405f77116971e7e
a115444 <q115444@gmail.com>
2018-06-13 12:20:39 +0800

    add user name to registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
+  validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
 
+
+  before_save :initialize_name
   
+  # 如果 name 沒有值的話, 請初始化一個值給它, 而這個初始化的值是 user 填的 email @前面的字串
+  def initialize_name
+    if self.name == '' || self.name == nil
+      self.name = self.email.split('@').first
+    end
+  end
+
 
   def admin?
     self.role == "admin"
