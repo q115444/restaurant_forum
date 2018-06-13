bb1a011ad1e1bcebf8a8922a05857f05c5629219
a115444 <q115444@gmail.com>
2018-06-13 10:09:44 +0800

    setup route for user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :categories, only: :show
+  resources :categories, only: :show
 
 
   resources :restaurants, only:[:index, :show] do 
  
-     resources :comments, only: [:create, :destroy] 
+    resources :comments, only: [:create, :destroy] 
+  
   end  
- 
+  
+  resources :users,only: [:show, :edit, :update]
+
  root "restaurants#index"
  
  namespace :admin do
