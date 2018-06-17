e5e6b8fa8f50c548748fa84cf23da61d99d1fa4a
a115444 <q115444@gmail.com>
2018-06-18 02:21:29 +0800

    create favorite model and setup relation among user, restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,12 @@
 class Restaurant < ApplicationRecord
   has_many :comments, dependent: :destroy
 
+ # 「餐廳被很多使用者收藏」的多對多關聯
+   # 將關聯名稱自訂為 :favorited_users
+   # 自訂名稱後，Rails 無法自動推論來源名稱，需另加 source 告知 model name
+   has_many :favorites, dependent: :destroy
+   has_many :favorited_users, through: :favorites, source: :user
+
   mount_uploader :image, PhotoUploader
   #mount_uploader :image, PhotoImageUploader
   validates_presence_of :name
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  #使用者評論很多餐廳 多對多關聯
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
+  
+  ＃使用者收藏很多餐廳
+  has_many :favorites, dependent: :destroy
+  has_many :favorites_restaurants, through: :favorites, source: :restaurant
+                                                        #source 指的是model name
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
