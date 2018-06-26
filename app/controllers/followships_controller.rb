1feae405ab7f9b37c22208bbc6cd3bd915413b0e
a115444 <q115444@gmail.com>
2018-06-26 14:36:03 +0800

    add unfollow button on user index page and destroy action

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -8,8 +8,17 @@ class FollowshipsController < ApplicationController
         redirect_back(fallback_location: root_path)
     else
         flash[:alert] = @followship.errors.full_messages.to_sentence
-        redirect_back(fallback_location: root_path) 
+        redirect_back(fallback_location: root_path)
+        # 驗證失敗時，Model 將錯誤訊息放在 errors 裡回傳
+        # 使用 errors.full_messages 取出完成訊息集合(Array)，串接 to_sentence 將 Array 組合成 String 
     end       
   end       
 
+
+  def destroy
+    @followship = current_user.followships.where(following_id: params[:id]).first
+    @followship.destroy
+    flash[:alert] = "Followship destroyed"
+    redirect_back(fallback_location: root_path)
+  end
 end
